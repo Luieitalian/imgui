10b5432a827b0f33a82e900bc5ff41d5936bd930
Luieitalian <enderbud125@hotmail.com>
2024-08-24 19:50:18 +0300

    Added premake

---
+++ premake5.lua
@@ -0,0 +1,27 @@
+project "ImGui"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+    location "%{wks.location}/ImGui"
+    targetdir "%{wks.location}/ImGui/bin/%{cfg.buildcfg}/"
+
+    objdir "%{wks.location}/ImGui/obj/%{cfg.buildcfg}/"
+
+    files
+    {
+      "*.h", "*.cpp"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+        staticruntime "On"
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        symbols "On"
+        runtime "Debug"
+
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        optimize "On"
+        runtime "Release"
